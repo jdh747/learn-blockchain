b637f4d26a15b8f35073d52693d16f76a9b64fec
Joshua Drew Hayes <joshhayes747@gmail.com>
2018-06-03 16:45:25 +1200

    Now adds new transactions to current block

--- blockchain.py
+++ blockchain.py
@@ -7,9 +7,23 @@ class Blockchain(object):
         # Create a new block and add it to the chain
         pass
 
-    def new_transaction(self):
-        # Add a new transaction to current current_transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined block.
+        
+        Arguments:
+            sender {str} -- Address of the sender.
+            recipient {str} -- Address of the recipient.
+            amount {int} -- Amount being transferred.
+            return {int} -- The index of the block that will hold this transaction.
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
